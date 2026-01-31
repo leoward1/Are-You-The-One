d5974a1417e1572bd470c064ff7fb704ebf9f244
mennrinn <leoward1926@gmail.com>
2026-01-31 13:13:26 +0100

    Remove Stripe to fix iOS build - can be re-added later

--- App.tsx
+++ App.tsx
@@ -2,14 +2,11 @@ import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { StripeProvider } from '@stripe/stripe-react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet } from 'react-native';
 
 import RootNavigator from './src/navigation/RootNavigator';
 import { useAuthStore } from './src/store';
-
-const STRIPE_PUBLISHABLE_KEY = process.env.STRIPE_PUBLISHABLE_KEY || '';
 
 export default function App() {
   const loadUser = useAuthStore((state: any) => state.loadUser);
@@ -21,12 +18,10 @@ export default function App() {
   return (
     <GestureHandlerRootView style={styles.container}>
       <SafeAreaProvider>
-        <StripeProvider publishableKey={STRIPE_PUBLISHABLE_KEY}>
-          <NavigationContainer>
-            <RootNavigator />
-          </NavigationContainer>
-          <StatusBar style="auto" />
-        </StripeProvider>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+        <StatusBar style="auto" />
       </SafeAreaProvider>
     </GestureHandlerRootView>
   );
